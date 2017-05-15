88938ee53db7b2bcec90627fb990da1df48ab968
AntonMiracle <b.anton.m.1986@gmail.com>
2017-05-15 14:21:49 +0300

    test buttons

--- src/main/webapp/resources/js/button.js
+++ src/main/webapp/resources/js/button.js
@@ -1,67 +1,68 @@
-$('.menuHome #clients').mouseenter(function() {
-	switchElementTextValueAndSize('.menuHome #clients', '20', 'CLIENTS');
-});
-$('.menuHome #clients').mouseleave(function() {
-	switchElementTextValueAndSize('.menuHome #clients', '25', 'C');
-});
-$('.menuHome #proposals').mouseenter(function() {
-	switchElementTextValueAndSize('.menuHome #proposals', '19', 'PROPOSALS');
-});
-$('.menuHome #proposals').mouseleave(function() {
-	switchElementTextValueAndSize('.menuHome #proposals', '25', 'P');
-});
-$('.menuHome #workers').mouseenter(function() {
-	switchElementTextValueAndSize('.menuHome #workers', '20', 'WORKERS');
-});
-$('.menuHome #workers').mouseleave(function() {
-	switchElementTextValueAndSize('.menuHome #workers', '25', 'W');
-});
-$('.menuHome #newClient').mouseenter(function() {
-	switchElementTextValueAndSize('.menuHome #newClient', '20', 'NEW');
-});
-$('.menuHome #newClient').mouseleave(function() {
-	switchElementTextValueAndSize('.menuHome #newClient', '25', 'N');
-});
-$('.menuHome #allClients').mouseenter(function() {
-	switchElementTextValueAndSize('.menuHome #allClients', '20', 'ALL');
-});
-$('.menuHome #allClients').mouseleave(function() {
-	switchElementTextValueAndSize('.menuHome #allClients', '25', 'A');
-});
-$('.menuHome #newProposal').mouseenter(function() {
-	switchElementTextValueAndSize('.menuHome #newProposal', '20', 'NEW');
-});
-$('.menuHome #newProposal').mouseleave(function() {
-	switchElementTextValueAndSize('.menuHome #newProposal', '25', 'N');
-});
-$('.menuHome #allProposals').mouseenter(function() {
-	switchElementTextValueAndSize('.menuHome #allProposals', '20', 'ALL');
-});
-$('.menuHome #allProposals').mouseleave(function() {
-	switchElementTextValueAndSize('.menuHome #allProposals', '25', 'A');
-});
-$('.menuHome #newWorker').mouseenter(function() {
-	switchElementTextValueAndSize('.menuHome #newWorker', '20', 'NEW');
-});
-$('.menuHome #newWorker').mouseleave(function() {
-	switchElementTextValueAndSize('.menuHome #newWorker', '25', 'N');
-});
-$('.menuHome #allWorkers').mouseenter(function() {
-	switchElementTextValueAndSize('.menuHome #allWorkers', '20', 'ALL');
-});
-$('.menuHome #allWorkers').mouseleave(function() {
-	switchElementTextValueAndSize('.menuHome #allWorkers', '25', 'A');
-});
-$('.menuHome #tasks').mouseenter(function() {
-	switchElementTextValueAndSize('.menuHome #tasks', '20', 'TASKS');
-});
-$('.menuHome #tasks').mouseleave(function() {
-	switchElementTextValueAndSize('.menuHome #tasks', '25', 'T');
-});
-/* home menu username area */
-$('.menuHome #out').mouseenter(function() {
-	switchElementTextValueAndSize('.menuHome #out', '20', 'OUT');
-});
-$('.menuHome #out').mouseleave(function() {
-	switchElementTextValueAndSize('.menuHome #out', '25', 'O');
+$(document).ready(function() {	
+	$('.menuHome #clients').mouseenter(function() {
+		switchElementTextValueAndSize('.menuHome #clients', '20', 'CLIENTS');
+	});
+	$('.menuHome #clients').mouseleave(function() {
+		switchElementTextValueAndSize('.menuHome #clients', '25', 'C');
+	});
+	$('.menuHome #proposals').mouseenter(function() {
+		switchElementTextValueAndSize('.menuHome #proposals', '19', 'PROPOSALS');
+	});
+	$('.menuHome #proposals').mouseleave(function() {
+		switchElementTextValueAndSize('.menuHome #proposals', '25', 'P');
+	});
+	$('.menuHome #workers').mouseenter(function() {
+		switchElementTextValueAndSize('.menuHome #workers', '20', 'WORKERS');
+	});
+	$('.menuHome #workers').mouseleave(function() {
+		switchElementTextValueAndSize('.menuHome #workers', '25', 'W');
+	});
+	$('.menuHome #newClient').mouseenter(function() {
+		switchElementTextValueAndSize('.menuHome #newClient', '20', 'NEW');
+	});
+	$('.menuHome #newClient').mouseleave(function() {
+		switchElementTextValueAndSize('.menuHome #newClient', '25', 'N');
+	});
+	$('.menuHome #allClients').mouseenter(function() {
+		switchElementTextValueAndSize('.menuHome #allClients', '20', 'ALL');
+	});
+	$('.menuHome #allClients').mouseleave(function() {
+		switchElementTextValueAndSize('.menuHome #allClients', '25', 'A');
+	});
+	$('.menuHome #newProposal').mouseenter(function() {
+		switchElementTextValueAndSize('.menuHome #newProposal', '20', 'NEW');
+	});
+	$('.menuHome #newProposal').mouseleave(function() {
+		switchElementTextValueAndSize('.menuHome #newProposal', '25', 'N');
+	});
+	$('.menuHome #allProposals').mouseenter(function() {
+		switchElementTextValueAndSize('.menuHome #allProposals', '20', 'ALL');
+	});
+	$('.menuHome #allProposals').mouseleave(function() {
+		switchElementTextValueAndSize('.menuHome #allProposals', '25', 'A');
+	});
+	$('.menuHome #newWorker').mouseenter(function() {
+		switchElementTextValueAndSize('.menuHome #newWorker', '20', 'NEW');
+	});
+	$('.menuHome #newWorker').mouseleave(function() {
+		switchElementTextValueAndSize('.menuHome #newWorker', '25', 'N');
+	});
+	$('.menuHome #allWorkers').mouseenter(function() {
+		switchElementTextValueAndSize('.menuHome #allWorkers', '20', 'ALL');
+	});
+	$('.menuHome #allWorkers').mouseleave(function() {
+		switchElementTextValueAndSize('.menuHome #allWorkers', '25', 'A');
+	});
+	$('.menuHome #tasks').mouseenter(function() {
+		switchElementTextValueAndSize('.menuHome #tasks', '20', 'TASKS');
+	});
+	$('.menuHome #tasks').mouseleave(function() {
+		switchElementTextValueAndSize('.menuHome #tasks', '25', 'T');
+	});
+	$('.menuHome #out').mouseenter(function() {
+		switchElementTextValueAndSize('.menuHome #out', '20', 'OUT');
+	});
+	$('.menuHome #out').mouseleave(function() {
+		switchElementTextValueAndSize('.menuHome #out', '25', 'O');
+	});
 });
